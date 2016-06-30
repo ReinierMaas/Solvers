b55423c4fc0092df7ae8fa29782deb5ed14f5864
Reinier Remco Maas <ReinierMaas@hotmail.com>
2016-06-30 02:33:22 +0200

    Added time measurement to the console average ms

--- Solvers/Solvers/Program.cs
+++ Solvers/Solvers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Solvers
 {
@@ -6,14 +7,21 @@ namespace Solvers
     {
         static void Main(string[] args)
         {
+            Stopwatch stopwatch = new Stopwatch();
+            long elapsedTime = 0;
+            int count = 0;
             foreach (string s in Sudoku.sudokuInput9)
             {
                 Sudoku sudoku = new Sudoku();
                 Sudoku.Board board = sudoku.Read(9, s);
                 Console.Write(board);
+                stopwatch.Restart();
                 sudoku.SolveBinaryHeap(board);
+                elapsedTime += stopwatch.ElapsedMilliseconds;
+                count++;
                 Console.Write(board);
             }
+            Console.WriteLine($"Time: {elapsedTime} ms for {count} sudokus, average {elapsedTime / count} ms");
             Console.ReadKey();
         }
     }
